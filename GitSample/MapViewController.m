8f5ea7cbc4c2a8485fb5ce3f62246c0a1b919641
kalaifarshore <kalaivanan@farshore.com>
2016-08-19 17:23:37 +0530

    This is map feature

---
+++ GitSample/MapViewController.m
@@ -0,0 +1,37 @@
+//
+//  MapViewController.m
+//  GitSample
+//
+//  Created by dev11 on 8/19/16.
+//  Copyright © 2016 dev13. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@interface MapViewController ()
+
+@end
+
+@implementation MapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
