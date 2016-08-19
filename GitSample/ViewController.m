768e3c5b684db9a417f88be70f9bafd5b3a588d7
kalaifarshore <kalaivanan@farshore.com>
2016-08-19 19:47:05 +0530

    My first origin commit

--- GitSample/ViewController.m
+++ GitSample/ViewController.m
@@ -18,11 +18,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+     NSLog(@"pushed to origin master");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+   
     // Dispose of any resources that can be recreated.
 }
 - (IBAction)GitClicked:(id)sender {
